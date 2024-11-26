722756a2f17750156a3a3951a722dee4e22032ce
erinchen <erinrui.chen@mail.utoronto.ca>
2024-11-25 19:25:23 -0500

    updated GradeAccountDAO with loading and saving food methods. Commented out FoodLoggingStorageDAO.

--- src/main/java/data_access/GradeAccountDAO.java
+++ src/main/java/data_access/GradeAccountDAO.java
@@ -2,16 +2,23 @@ package data_access;
 
 import api.GradeHelper;
 import data.AccountInfo;
+import data.DayInfo;
+import data.Food;
 import okhttp3.*;
 import org.json.JSONObject;
+import use_case.food_logging.DataAccessException;
+import use_case.food_logging.LogFoodDataAccessInterface;
 import use_case.login.LoginDataAccessInterface;
 import use_case.logout.LogoutDataAccessInterface;
 import use_case.signup.SignupDataAccessInterface;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GradeAccountDAO implements SignupDataAccessInterface, LoginDataAccessInterface,
-        LogoutDataAccessInterface {
+        LogoutDataAccessInterface, LogFoodDataAccessInterface {
 
     public void createAccount(String username, String password) {
         GradeHelper.postUser(username, password);
@@ -45,5 +52,46 @@ public class GradeAccountDAO implements SignupDataAccessInterface, LoginDataAcce
     @Override
     public void setCurrentUsername(String username) {
 
+    }
+
+    //saves the food information for current day
+    @Override
+    public void saveFood(String username, String password, Food foodIntake){
+        AccountInfo user = GradeHelper.getUser(username);
+        List<DayInfo> days = user.getDays();
+        boolean dayInMemory = false;
+        for (DayInfo day: days){
+            if (day.getDate().equals(LocalDate.now())){
+                List<Food> currFoodLog = day.getFoodLog();
+                List<Food> newFoodLog = new ArrayList<>();
+                for (Food dayFood: currFoodLog){
+                    newFoodLog.add(dayFood);
+                }
+                newFoodLog.add(foodIntake);
+                day.setFoodLog(newFoodLog);
+                dayInMemory = true;
+            }
+        }
+        if (dayInMemory == false){
+            DayInfo newDay = new DayInfo(LocalDate.now());
+            ArrayList<Food> newDayFoodLog = new ArrayList<>();
+            newDayFoodLog.add(foodIntake);
+            newDay.setFoodLog(newDayFoodLog);
+        }
+        GradeHelper.setUserInfo(user.getUsername(), user.getPassword(), user);
+    }
+
+    //loads the food information for requested date
+    @Override
+    public List<Food> loadFoodInfo(String username, LocalDate date){
+        AccountInfo user = GradeHelper.getUser(username);
+        List<DayInfo> days = user.getDays();
+        boolean dayInMemory = false;
+        for (DayInfo day: days){
+            if (day.getDate().equals(LocalDate.now())){
+                return day.getFoodLog();
+            }
+        }
+        return null;
     }
 }
--- src/main/java/use_case/food_logging/LogFoodDataAccessInterface.java
+++ src/main/java/use_case/food_logging/LogFoodDataAccessInterface.java
@@ -3,8 +3,11 @@ import data.AccountInfo;
 import data.Food;
 import org.json.JSONObject;
 
-public interface LogFoodDataAccessInterface {
-    JSONObject saveFood (String userName, String password, Food foodIntake) throws DataAccessException;
+import java.time.LocalDate;
+import java.util.List;
 
-    JSONObject loadFoodInfo(String userName) throws DataAccessException;
+public interface LogFoodDataAccessInterface {
+    void saveFood (String userName, String password, Food foodIntake) throws DataAccessException;
+
+    List<Food> loadFoodInfo(String userName, LocalDate date) throws DataAccessException;
 }
