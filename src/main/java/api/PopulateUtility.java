4940dcabe0bb3cade2e14bf70145355e7ecb6d8e
matts1mpson <matt.simpson@mail.utoronto.ca>
2024-11-29 17:14:10 -0500

    Refactored method from FoodDataCentralPopulateDAO into a utility class since the method does not directly call the API. It simply produces a Food entity from a JSONObject that is produced by a method that directly calls the FDC API. Began work on micronutrient support functionality, although not very much.

--- src/main/java/api/PopulateUtility.java
+++ src/main/java/api/PopulateUtility.java
@@ -75,6 +75,13 @@ public class PopulateUtility {
         return temp;
     }
 
+    public static HashMap<String, Object> tempGenerator(JSONArray fresNutrients, int i, String microsYES) {
+        HashMap<String, Object> temp = new HashMap<>();
+        temp.put("amount per 100", fresNutrients.getJSONObject(i).getDouble("amount"));
+        temp.put("unitName", fresNutrients.getJSONObject(i).getString("unitName"));
+        return temp;
+    }
+
     public static Food createFood(JSONObject result) {
         String description = result.getString("description");
         JSONArray fresNutrients = result.getJSONArray("foodNutrients");
@@ -123,13 +130,78 @@ public class PopulateUtility {
         return foodTbc;
     }
 
+    public static Food createFood(JSONObject result, String microsYES) {
+        String description = result.getString("description");
+        JSONArray fresNutrients = result.getJSONArray("foodNutrients");
+        // foodTbc is short for foodToBeCreated.
+        Food foodTbc = new Food();
+        foodTbc.setDescription(description);
+        // Loop through until we've found protein, carbs, fat, and calories amounts per 100 standard units.
+        // Record the units of these as well.
+        int i = 0;
+        int j = 0;
+        HashMap<String, HashMap<String, Object>> macros = new HashMap<>();
+        HashMap<String, HashMap<String, Object>> micros = new HashMap<>();
+        while (i < fresNutrients.length() && j >= 0) {
+            String nutrientName = fresNutrients.getJSONObject(i).getString("name");
+            String unitName = fresNutrients.getJSONObject(i).getString("unitName");
+            if (nutrientName.equals("Protein")) {
+                HashMap<String, Object> temp = tempGenerator(fresNutrients, i);
+                macros.put("Protein", temp);
+                i += 1;
+                j += 1;
+            }
+            else if (nutrientName.equals("Carbohydrate, by difference")) {
+                HashMap<String, Object> temp = tempGenerator(fresNutrients, i);
+                macros.put("Carbohydrate", temp);
+                i += 1;
+                j += 1;
+            }
+            else if (unitName.equals("KCAL") && (nutrientName.equals("Energy") || nutrientName.contains("Energy"))) {
+                HashMap<String, Object> temp = tempGenerator(fresNutrients, i);
+                foodTbc.setCalories(temp);
+                i += 1;
+                j += 1;
+            }
+            else if (nutrientName.equals("Total lipid (fat)")) {
+                HashMap<String, Object> temp = tempGenerator(fresNutrients, i);
+                macros.put("Fat", temp);
+                i += 1;
+                j += 1;
+            }
+            else if (nutrientName.equals("Vitamin A IU")) {
+                HashMap<String, Object> temp = tempGenerator(fresNutrients, i);
+                micros.put("Vitamin A IU", temp);
+                i += 1;
+                j += 1;
+            }
+            else {
+                i += 1;
+            }
+
+
+        }
+        foodTbc.setMacroNutrients(macros);
+        return foodTbc;
+    }
+
+
     public static void main(String[] args) {
-        FoodDataCentralSearchDAO usdaObj = new FoodDataCentralSearchDAO(genMyApiKey("myFDCApiKey.txt"));
+/*        FoodDataCentralSearchDAO usdaObj = new FoodDataCentralSearchDAO(genMyApiKey("myFDCApiKey.txt"));
         Integer[] array = {203,204,205,208, 268, 957, 958};
         //JSONObject result = usdaObj.getFoodByFdcId(173410, array);
         JSONObject result = usdaObj.getFoodByFdcId(2514744, array);
         Food newFood = createFood(result);
-        int i7 = 7;
+        int i7 = 7;*/
+        FoodDataCentralSearchDAO usdaObj = new FoodDataCentralSearchDAO(genMyApiKey("myFDCApiKey.txt"));
+        // Menaquinone-4=428
+        Integer[] MACRO_AND_ESSENTIAL_MICROS = {203, 204, 205, 208, 268, 957, 318, 401, 324, 323, 430, 428};
+        JSONObject result = usdaObj.getFoodByFdcId(2514744, MACRO_AND_ESSENTIAL_MICROS);
+        int y = 0;
+        HashMap<String, Integer> foodMap = usdaObj.searchComplexFood("kale");
+        JSONObject result2 = usdaObj.getFoodByFdcId(174730, MACRO_AND_ESSENTIAL_MICROS);
+        JSONObject result23 = usdaObj.getFoodByFdcId(168421, MACRO_AND_ESSENTIAL_MICROS);
+        int yyyy = 6;
     }
     }
 
